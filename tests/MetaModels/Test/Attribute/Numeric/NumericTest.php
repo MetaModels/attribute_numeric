071e919d2da5d62570f18d45126c87976261181e
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-26 02:19:25 +0100

    Implement searchFor() (fixes MetaModel/core#957)

--- tests/MetaModels/Test/Attribute/Numeric/NumericTest.php
+++ tests/MetaModels/Test/Attribute/Numeric/NumericTest.php
@@ -21,7 +21,9 @@
 
 namespace MetaModels\Test\Attribute\Numeric;
 
+use Contao\Database;
 use MetaModels\Attribute\Numeric\Numeric;
+use MetaModels\MetaModelsServiceContainer;
 
 /**
  * Unit tests to test class Numeric.
@@ -29,14 +31,102 @@ use MetaModels\Attribute\Numeric\Numeric;
 class NumericTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Mock the Contao database.
+     *
+     * @param string     $expectedQuery The query to expect.
+     *
+     * @param null|array $result        The resulting datasets.
+     *
+     * @return Database|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function mockDatabase($expectedQuery = '', $result = null)
+    {
+        $mockDb = $this
+            ->getMockBuilder('Contao\Database')
+            ->disableOriginalConstructor()
+            ->setMethods(array('__destruct'))
+            ->getMockForAbstractClass();
+
+        $mockDb->method('createStatement')->willReturn(
+            $statement = $this
+                ->getMockBuilder('Contao\Database\Statement')
+                ->disableOriginalConstructor()
+                ->setMethods(array('debugQuery', 'createResult'))
+                ->getMockForAbstractClass()
+        );
+
+        if (!$expectedQuery) {
+            $statement->expects($this->never())->method('prepare_query');
+
+            return $mockDb;
+        }
+
+        $statement
+            ->expects($this->once())
+            ->method('prepare_query')
+            ->with($expectedQuery)
+            ->willReturnArgument(0);
+
+        if ($result === null) {
+            $result = array('ignored');
+        } else {
+            $result = (object) $result;
+        }
+
+        $statement->method('execute_query')->willReturn($result);
+        $statement->method('createResult')->willReturnCallback(
+            function ($resultData) {
+                $index = 0;
+
+                $resultData = (array) $resultData;
+
+                $resultSet = $this
+                    ->getMockBuilder('Contao\Database\Result')
+                    ->disableOriginalConstructor()
+                    ->getMockForAbstractClass();
+
+                $resultSet->method('fetch_row')->willReturnCallback(function () use (&$index, $resultData) {
+                    return array_values($resultData[$index++]);
+                });
+                $resultSet->method('fetch_assoc')->willReturnCallback(function () use (&$index, $resultData) {
+                    if (!isset($resultData[$index])) {
+                        return false;
+                    }
+                    return $resultData[$index++];
+                });
+                $resultSet->method('num_rows')->willReturnCallback(function () use ($index, $resultData) {
+                    return count($resultData);
+                });
+                $resultSet->method('num_fields')->willReturnCallback(function () use ($index, $resultData) {
+                    return count($resultData[$index]);
+                });
+                $resultSet->method('fetch_field')->willReturnCallback(function ($field) use ($index, $resultData) {
+                    $data = array_values($resultData[$index]);
+                    return $data[$field];
+                });
+                $resultSet->method('data_seek')->willReturnCallback(function ($newIndex) use (&$index, $resultData) {
+                    $index = $newIndex;
+                });
+
+                return $resultSet;
+            }
+        );
+
+        return $mockDb;
+    }
+
+    /**
      * Mock a MetaModel.
      *
-     * @param string $language         The language.
-     * @param string $fallbackLanguage The fallback language.
+     * @param string   $language         The language.
+     *
+     * @param string   $fallbackLanguage The fallback language.
+     *
+     * @param Database $database         The database to use.
      *
      * @return \MetaModels\IMetaModel
      */
-    protected function mockMetaModel($language, $fallbackLanguage)
+    protected function mockMetaModel($language, $fallbackLanguage, $database)
     {
         $metaModel = $this->getMock(
             'MetaModels\MetaModel',
@@ -59,6 +149,13 @@ class NumericTest extends \PHPUnit_Framework_TestCase
             ->method('getFallbackLanguage')
             ->will($this->returnValue($fallbackLanguage));
 
+        $serviceContainer = new MetaModelsServiceContainer();
+        $serviceContainer->setDatabase($database);
+
+        $metaModel
+            ->method('getServiceContainer')
+            ->willReturn($serviceContainer);
+
         return $metaModel;
     }
 
@@ -69,7 +166,88 @@ class NumericTest extends \PHPUnit_Framework_TestCase
      */
     public function testInstantiation()
     {
-        $text = new Numeric($this->mockMetaModel('en', 'en'));
+        $text = new Numeric($this->mockMetaModel('en', 'en', $this->mockDatabase()));
         $this->assertInstanceOf('MetaModels\Attribute\Numeric\Numeric', $text);
+    }
+
+
+    /**
+     * Test provider for testSearchFor().
+     *
+     * @return array
+     */
+    public function searchForProvider()
+    {
+        return array(
+            array('10'),
+            array(10),
+        );
+    }
+
+    /**
+     * Test the searchFor() method.
+     *
+     * @param string|int|float $value The value to search.
+     *
+     * @return void
+     *
+     * @dataProvider searchForProvider
+     */
+    public function testSearchFor($value)
+    {
+        $decimal = new Numeric(
+            $this->mockMetaModel(
+                'en',
+                'en',
+                $this->mockDatabase(
+                    'SELECT id FROM mm_unittest WHERE test=?',
+                    array(array('id' => 1), array('id' => 2))
+                )
+            ),
+            array('colname' => 'test')
+        );
+
+        $this->assertEquals(array(1, 2), $decimal->searchFor($value));
+    }
+
+    /**
+     * Test the searchFor() method with a wildcard.
+     *
+     * @return void
+     */
+    public function testSearchForWithWildcard()
+    {
+        $decimal = new Numeric(
+            $this->mockMetaModel(
+                'en',
+                'en',
+                $this->mockDatabase(
+                    'SELECT id FROM mm_unittest WHERE test LIKE ?',
+                    array(array('id' => 1), array('id' => 2))
+                )
+            ),
+            array('colname' => 'test')
+        );
+
+        $this->assertEquals(array(1, 2), $decimal->searchFor('10*'));
+    }
+
+    /**
+     * Test the searchFor() method with a non numeric value.
+     *
+     * @return void
+     */
+    public function testSearchForWithNonNumeric()
+    {
+        $decimal = new Numeric(
+            $this->mockMetaModel(
+                'en',
+                'en',
+                $this->mockDatabase()
+            ),
+            array('colname' => 'test')
+        );
+
+        $this->assertEquals(array(), $decimal->searchFor('abc'));
     }
 }
