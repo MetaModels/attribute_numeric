646c85f9545642975850b9df196d77fcdbdbcb16
David Molineus <david.molineus@netzmacht.de>
2017-10-19 11:17:18 +0200

    Provide basic bundle structure.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_text.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeNumeric
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\Attribute\Numeric\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use MetaModels\Attribute\Numeric\MetaModelsAttributeNumericBundle;
+use MetaModels\CoreBundle\MetaModelsCoreBundle;
+
+class Plugin  implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MetaModelsAttributeNumericBundle::class)
+                ->setLoadAfter(
+                    [
+                        MetaModelsCoreBundle::class
+                    ]
+                )
+                ->setReplace(['metamodels'])
+        ];
+    }
+}
